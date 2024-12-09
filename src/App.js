85500e4be2cfe8a33cc215286b40fe52437275c6
razmiqayelyan <raz.miqayelyan@gmail.com>
2024-12-09 11:16:19 -0800

    Create Authentification and make pages private

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Category from './pages/Category';
 import Detail from './pages/Detail';
+import Login from './pages/Login';
+import PrivateRoute from './components/PrivateRoute';
 import NotFound from './components/NotFound';
-import './styles/App.css'
 
 const App = () => (
-  <Router>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/:category" element={<Category />} />
-      <Route path="/:category/:id" element={<Detail />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </Router>
+  <AuthProvider>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/:category"
+          element={
+            <PrivateRoute>
+              <Category />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/:category/:id"
+          element={
+            <PrivateRoute>
+              <Detail />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  </AuthProvider>
 );
 
 export default App;
